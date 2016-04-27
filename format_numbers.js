76b318a2d48333784bf72d00538095fc392e7cd6
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2016-04-27 18:29:00 +0100

    ready for deployment

--- format_numbers.js
+++ format_numbers.js
@@ -1,37 +1,34 @@
 
-// Helper function to guarantee cross-browser compatibility
-// adapted from: http://stackoverflow.com/a/16157942
-function localeString(x, sep, grp) {
-  var sx = (''+x).split('.'), s = '', i, j;
-  sep || (sep = ',');            // default separator
-  grp || grp === 0 || (grp = 3); // default grouping
-  i = sx[0].length;
-  while (i > grp) {
-    j = i - grp;
-    s = sep + sx[0].slice(j, i) + s;
-    i = j;
-  }
-  s = sx[0].slice(0, i) + s;
-  sx[0] = s;
-  return sx.join('.');
-}
-
-var firstTime = true;
-
 $(document).ready(function() {
+  /**************************** OPTION 1 ****************************/
+  // No JS needed!!
+  
+  /**************************** OPTION 2 ****************************/
+  // Helper function to guarantee cross-browser compatibility
+  // adapted from: http://stackoverflow.com/a/16157942
+  function localeString(x, sep, grp) {
+    var sx = (''+x).split('.'), s = '', i, j;
+    sep || (sep = ',');            // default separator
+    grp || grp === 0 || (grp = 3); // default grouping
+    i = sx[0].length;
+    while (i > grp) {
+      j = i - grp;
+      s = sep + sx[0].slice(j, i) + s;
+      i = j;
+    }
+    s = sx[0].slice(0, i) + s;
+    sx[0] = s;
+    return sx.join('.');
+  }
+  
+  // To change n2's input field (lose arrows and other functionality)
+  $('#n2')[0].type = 'text';
   
   // To format the number when the app starts up
-    $(document).on('shiny:value', function(event) {
-      if (firstTime && event.name === 'prettyN') {
-        event.value = localeString(event.value);
-        firstTime = false;
-      } else return;
-    });
+  $('#n2').val(localeString($('#n2').val()));
   
   // To format the number whenever the input changes
-  $(document).on('shiny:inputchanged', function(event) {
-    if (event.name === 'n') {
-      event.value = localeString(event.value);
-    }
+  $('#n2').keyup(function(event) {
+    $(this).val(localeString($(this).val().replace(/,/g, '')));
   });
 });
--- server.R
+++ server.R
@@ -1,6 +1,14 @@
 library(shiny)
 
 shinyServer(function(input, output, session) {
-  output$prettyN <- renderText({ input$n })
-  output$prettyFoo <- renderText({ input$foo })
+  #********** Option 1 **********#
+  output$numberN1 <- renderText({ input$n1 })
+  output$numberN1op <- renderText({ input$n1*2 })
+  output$formatN1 <- renderText({ formatC(input$n1, format="d", big.mark=',') })
+  
+  #********** Option 2 **********#
+  n2 <- reactive({ as.numeric(gsub(",", "", input$n2)) })
+  output$formatN2 <- renderText({ input$n2 })
+  output$numberN2 <- renderText({ n2() })
+  output$numberN2op <- renderText({ n2()*2 })
 })
